513eeca6d650a2a24057552ccace0830c5e07029
vzceti <53797827+vzceti@users.noreply.github.com>
2020-12-20 17:58:36 -0500

    Update util2.R

--- util2.R
+++ util2.R
@@ -1,18 +1,22 @@
-# ctrl-F "MODIFY" for adding data or customizing to personal machine
 # load required packages, set data sources, track run time 
 a <- Sys.time()
 
-require(readxl)
 require(tidyverse)
 require(readr)
+require(tidyr)
+require(dplyr)
+require(reshape2)
+require(plyr)
+require(zoo)
+require(readxl)
 
-# MODIFY inputdata path
 inputdata <- "data2.csv"
+changes <- "changedates.xlsx"
 nytlink <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
 
 source("input.R")
 
-# TIME FOR feature engineering
+rm(list=setdiff(ls(), c("a", "schooldata")))
 
-
-Sys.time()-a
+a <- Sys.time()-a
+a
