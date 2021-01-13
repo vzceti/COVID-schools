04b08b03b0fc016c3c8c8218beeaa344801625b2
vzceti <53797827+vzceti@users.noreply.github.com>
2021-01-12 22:56:20 -0500

    Update util2.R

--- util2.R
+++ util2.R
@@ -1,14 +1,16 @@
 # load required packages, set data sources, track run time 
 a <- Sys.time()
 
-require(tidyverse)
-require(readr)
-require(tidyr)
-require(dplyr)
-require(reshape2)
-require(plyr)
-require(zoo)
-require(readxl)
+requireLibs <- function(libs) {
+  for (lib in libs){
+    if(!is.element(lib, .packages(all.available = TRUE))) {
+      install.packages(lib)
+    }
+    library(lib, character.only = TRUE)
+  }
+}
+requireLibs(c("tidyverse","readr","tidyr","dplyr","reshape2",
+              "plyr","zoo","readxl"))
 
 inputdata <- "data2.csv"
 changes <- "changedates.xlsx"
@@ -17,6 +19,7 @@ nytlink <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-co
 source("input.R")
 
 rm(list=setdiff(ls(), c("a", "schooldata")))
+view(schooldata)
 
 a <- Sys.time()-a
 a
