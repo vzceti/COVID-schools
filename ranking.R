79ab4e39fb12c5c8cf094463c29f57f29043d76b
vzceti <53797827+vzceti@users.noreply.github.com>
2021-03-10 14:01:33 -0500

    Update ranking.R

--- ranking.R
+++ ranking.R
@@ -1,32 +1,28 @@
-csbDf <- knotsAndSpikes(t_start = 0, t_end = 40, knots = c(14,20), spikes = c(14,20))
+csbDf <- knotsAndSpikes(t_start = 0, t_end = 40, knots = c(10,20,23), spikes = 19)
 
-training_period_start <- as.factor("2020-08-30")
-training_period_end <- as.factor("2020-02-22")
-Target.name <- "outcomes"
-feature.names <- c("case_avg", "absent_rate","dropout_rate","enrollment","daily_atten",
-                   "expend_federal", "expend_state","expenditures", "s_deaths",
-                   "s_cases")
-schooldata$s_cases <- as.factor(schooldata$s_cases)
-schooldata$s_deaths <- as.factor(schooldata$s_deaths)
-schooldata$case_avg <- as.factor(schooldata$case_avg)
-schooldata[] <- lapply(schooldata, function(x) as.factor(x))
-schooldata <- schooldata[-c(16,22)]
-schooldata$pred_logit <- rankfunc(schooldata) 
+input_start <- schooldata[which(schooldata$t == 20 ),]
 
-group_as <- "deciles"
-if(group_as == 'deciles'){
-  schooldata<- deciles(schooldata)
-  
-}else{
-  schooldata <- twentiles(schooldata)
-  
-}
+fit_start <- glm(status2 ~ trump + pop + case_avg + s_cases,
+                 data=input_start, family='gaussian')
 
-# ROC Curve
-par(pty = "s")
-print(roc(schooldata$outcomes, schooldata$p_outcomes, plot = TRUE,
-          legacy.axes = TRUE, print.auc = TRUE, col = "blue", lwd =1))
-#Set legend
-legend("bottomright", legend = "virtual", col= "blue", lwd = 1)
-
-view(schooldata)
+summary(fit_start)
+# 
+# scoring_data_strip$p_fail <- predict(fit_fail, newdata=scoring_data_strip, type='response')
+# 
+# input_retire <- scoring_data_strip[which(  scoring_data_strip$failure_event == 0 & scoring_data_strip$t <= 36
+#                                            & scoring_data_strip$TRAINING == 1),]
+# 
+# fit_retire <- glm(retired_event ~    t_csb_8 +  t_csb_22 +  t_csb_35 +
+#                     HGST + TOSHIBA + WEST_DIGIT + LT_1TB + LT_3TB + LT_5TB + LT_9TB + LT_12TB + 
+#                     YR_2016 + YR_2017 + YR_2018 + smart_1_normalized, data=input_retire, family='binomial')
+# 
+# summary(fit_retire)
+# 
+# rm(input_retire)
+# 
+# scoring_data_strip$p_retire <- predict(fit_retire, newdata=scoring_data_strip, type='response')
+# rm(fit_retire)
+# 
+# DBI::dbWriteTable(db,"SCORES_ALL",scoring_data_strip, overwrite = T, temporary = F)
+# 
+# rm(scoring_data_strip)
