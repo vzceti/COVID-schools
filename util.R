eb22880868dddcf8ae7c5fcc3cedab8c117c519f
vzceti <53797827+vzceti@users.noreply.github.com>
2021-03-10 14:02:05 -0500

    Update util.R

--- util.R
+++ util.R
@@ -1,16 +1,12 @@
-# load required packages, set data sources, track run time 
 a <- Sys.time()
 
 source("functions.R")
 
 requireLibs(c("tidyverse","readr","tidyr","dplyr","reshape2",
-              "plyr","zoo","readxl","pROC"))
+              "plyr","zoo","readxl","pROC","data.table", "MASS"))
 source("fe.R")
 
 source("ranking.R")
-
-# write.csv(schooldata, "output.csv")
-# rm(list=setdiff(ls(), c("a", "schooldata")))
 
 a <- Sys.time()-a
 a
